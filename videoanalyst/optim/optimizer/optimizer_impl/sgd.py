8a9c4a312e8434ac02991f95f1c7dab0c7b0ab7d
Zacks <ggg1000111@gmail.com>
2021-09-03 09:33:17 +0800

    use AdamW

--- videoanalyst/optim/optimizer/optimizer_impl/sgd.py
+++ videoanalyst/optim/optimizer/optimizer_impl/sgd.py
@@ -34,4 +34,31 @@ class SGD(OptimizerBase):
         self._optimizer = optim.SGD(params, **kwargs)
 
 
+@OPTIMIZERS.register
+class AdamW(OptimizerBase):
+    r"""
+    Tracking data sampler
+
+    Hyper-parameters
+    ----------------
+    """
+    extra_hyper_params = dict(
+        lr=0.1,
+        betas=(0.9, 0.999),
+        eps=1e-8,
+        weight_decay=0.01
+    )
+
+    def __init__(self, cfg: CfgNode, model: torch.nn.Module) -> None:
+        super(AdamW, self).__init__(cfg, model)
+
+    def update_params(self, ):
+        super(AdamW, self).update_params()
+        params = self._state["params"]
+        kwargs = self._hyper_params
+        valid_keys = self.extra_hyper_params.keys()
+        kwargs = {k: kwargs[k] for k in valid_keys}
+        self._optimizer = optim.AdamW(params, **kwargs)
+
+AdamW.default_hyper_params.update(AdamW.extra_hyper_params)
 SGD.default_hyper_params.update(SGD.extra_hyper_params)
